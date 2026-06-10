3a0be93126f516c5f33e84fc2807d6f368eee2fe
Agegnehu Shibiru <agegnehushibiru5@gmail.com>
2026-06-10 16:48:59 +0300

    Implement variable support in Runtime class
    Added variable support to the execute method for storing and referencing variable values.

--- runtime.js
+++ runtime.js
@@ -1,27 +1,29 @@
 class Runtime {
-
   execute(ast) {
-
     let output = "";
+    const variables = {}; // Memory block to store variable names and values
 
     for (let node of ast) {
-
       if (node.type === "PrintStatement") {
+        let valueToPrint = node.value;
 
-        output += node.value.replaceAll('"', '') + "\n";
+        // If the print statement is referencing a variable name
+        if (node.valueType === "IDENTIFIER") {
+          if (variables[node.value] !== undefined) {
+            valueToPrint = variables[node.value]; // Fetch value from memory
+          } else {
+            valueToPrint = `ReferenceError: ${node.value} is not defined`;
+          }
+        }
 
-      }
-
+        output += valueToPrint + "\n";
+      } 
       else if (node.type === "VariableDeclaration") {
-
-        // Variable support coming soon
-
+        // Store the variable name and its assigned value in memory
+        variables[node.name] = node.value;
       }
-
     }
 
     return output;
-
   }
-
 }
